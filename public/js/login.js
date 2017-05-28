dc59c89c06f1360099b17231607aea9b63d87736
Henry Pap <onkelhoy@gmail.com>
2017-05-28 14:39:38 +0200

    login updated

--- public/js/login.js
+++ public/js/login.js
@@ -22,33 +22,30 @@ function login(){
   }
   document.querySelector('div.submit.button').innerHTML = '<i class="spinner loading icon"></i>';
 
+  $.ajax({
+    method: 'post',
+    url: url,
+    data: {
+      company: usn,
+      password: psw
+    },
 
-  var http = new XMLHttpRequest();
-  http.open('POST', url, true);
-  http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  http.onload = function(){
-    document.querySelector('div.submit.button').innerHTML = 'Login';
-    if(http.readyState == 4 && http.status >= 200 && http.status <= 299) {
-      if (http.status == 200) { // oh a admin logged in
+    success: function(data, statusText, xhr){
+      if (xhr.status == 200) { // oh a admin logged in
         location.href = '/admin';
         settings.data.admin = true;
       }
-      else if (http.status == 203) location.href = '/user';
+      else if (xhr.status == 203) location.href = '/user';
       $.ajax(settings);
-    }
-    else {
+    },
+
+    error: function(xhr){
+      console.log(xhr);
       if(http.status == 404) document.querySelector('div.ui.error.message').innerHTML = 'Username or Password is incorrect';
-      else document.querySelector('div.ui.error.message').innerHTML = http.responseText;
+      else document.querySelector('div.ui.error.message').innerHTML = 'unknown error, check console';
 
       // and display it as well
       document.querySelector('div.ui.error.message').style.display = 'block';
     }
-  }
-  http.onerror = function(xhr){
-    document.querySelector('div.submit.button').innerHTML = 'Login';
-    console.log(xhr);
-    document.querySelectorAll('div.ui.error.message').innerHTML = 'Too long response time';
-    document.querySelector('div.ui.error.message').style.display = 'block';
-  }
-  http.send(JSON.stringify({company:usn,password:psw}));
+  });
 }
